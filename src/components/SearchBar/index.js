b3793d713bf03e7df8bca1c9ad458dae3fe952e8
Ismartyx <Saeedojari@gmail.com>
2022-07-24 01:03:45 +0430

    fix SVG style

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,24 +1,27 @@
-import React from 'react'; 
-import UseAnimations from "react-useanimations";
-import searchToX from 'react-useanimations/lib/searchToX';
-import styles from './searchbar.module.css';
+import React from "react";
+import styles from "./searchbar.module.css";
 
 const SearchBar = () => {
-    const [query, setQuery] = React.useState("")
-    // function handleSearch() {
-    //     return <UseAnimations animation={searchToX} size={56} wrapperStyle={{ padding: 100 }} />
-    // }
+  const [query, setQuery] = React.useState("");
+  // function handleSearch() {
+  //     return <UseAnimations animation={searchToX} size={56} wrapperStyle={{ padding: 100 }} />
+  // }
 
-    return (
-        <div>
-            <form>
-                <span>
-                    <input  className={styles.search} type="search" placeholder="Search" onChange={event => setQuery(event.target.value)} />
-                    <p>{query ? <small>Nice search for{query}</small> : ""}</p>
-                 </span>
-             </form>
-        </div>
-    )
-}
+  return (
+    <div>
+      <form>
+        <span>
+          <input
+            className={styles.search}
+            type="search"
+            placeholder="Search"
+            onChange={(event) => setQuery(event.target.value)}
+          />
+          <p>{query ? <small>Nice search for{query}</small> : ""}</p>
+        </span>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
