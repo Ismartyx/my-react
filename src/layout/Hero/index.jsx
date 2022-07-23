17e6dca4812bdec643c00fd786c0e19e2be650ea
Ismartyx <Saeedojari@gmail.com>
2022-07-24 01:27:32 +0430

    remove useless fragment

--- src/layout/Hero/index.jsx
+++ src/layout/Hero/index.jsx
@@ -1,19 +1,16 @@
 import GetStart from "../../components/GetStart";
-import classes from './hero.module.css'
+import classes from "./hero.module.css";
 
 const Hero = () => {
-    return (
-        <>
-            <div className={classes.hero}>
-                <div className={classes.title}>
-                    <h1>React</h1>
-                    <p>A JavaScript library for building user interfaces</p>
-                </div>
-                <GetStart />
-
-            </div>
-        </>
-    )
-}
+  return (
+    <div className={classes.hero}>
+      <div className={classes.title}>
+        <h1>React</h1>
+        <p>A JavaScript library for building user interfaces</p>
+      </div>
+      <GetStart />
+    </div>
+  );
+};
 
 export default Hero;
