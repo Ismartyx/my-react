376c1960da69914b6cdac6f072b1a37aa7b7517a
Ismartyx <Saeedojari@gmail.com>
2022-07-24 01:16:14 +0430

    fix: change js files to jsx

---
+++ src/components/Homepage/index.jsx
@@ -0,0 +1,19 @@
+import styles from "./header.module.css";
+
+const imgSrc =
+  "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K";
+
+const Homepage = () => {
+  return (
+    <a
+      className={styles.logo}
+      style={{ textDecoration: "none", width: "calc(100% / 6)" }}
+      href="/_"
+    >
+      <img style={{ height: "20px" }} src={imgSrc} alt="logo"></img>
+      <span className={styles.span}>React</span>
+    </a>
+  );
+};
+
+export default Homepage;
