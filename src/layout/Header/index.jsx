975f07543c040d0a8bbd72eef8dbe463232931ae
Ismartyx <Saeedojari@gmail.com>
2022-07-24 01:21:26 +0430

    fix code style

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -1,18 +1,18 @@
 import Links from "../../components/Links";
 import Homepage from "../../components/Homepage";
-import styles from './header.module.css';
+import styles from "./header.module.css";
 import SearchBar from "../../components/SearchBar";
 import Information from "../../components/Information";
 
 const HeaderComp = () => {
-    return (
-            <div className={styles.header}>
-                <Homepage />
-                <Links />
-                <SearchBar />
-                <Information />
-            </div>
-    )
-}
+  return (
+    <div className={styles.header}>
+      <Homepage />
+      <Links />
+      <SearchBar />
+      <Information />
+    </div>
+  );
+};
 
 export default HeaderComp;
